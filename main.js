2c59471a240c46a7211e2bd9a6b3a3961c88196f
Benedek Biró <go98cub@mytum.de>
2024-03-17 17:36:00 +0100

    form correction

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const scrollButton = document.getElementById("scrollrsvp");
 const targetSection = document.getElementById("rsvp");
-const iframe = document.getElementById("google-form");
 
 scrollButton.addEventListener("click", function() {
   targetSection.scrollIntoView({
